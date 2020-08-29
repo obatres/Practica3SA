258f60ce3e5da1c5207f2b04878f0626dde1c007
Orlando <batres.93@gmail.com>
2020-08-28 18:23:55 -0600

    Solicitud del estado del pedido del cliente

--- Cliente/Cliente.py
+++ Cliente/Cliente.py
@@ -7,6 +7,7 @@ from datetime import datetime
 
 # Archivo de Logs
 Log=open("LogCliente.txt","w") 
+
 #-----------------------------Solicitar pedido al restaurante---------------
 # URL para solicitar pedido
 urlSolicitarPedido = "http://localhost:51630/api/pedido/"
@@ -18,6 +19,19 @@ response = requests.get(urlSolicitarPedido+idCliente)
 now = datetime.now()
 # Log del pedido solicitado
 Log.write(str(now)+" "+"Pedido solicitado "+str(response.json())+" "+"\n") 
+
+
+#-----------------------------Solicitar estado del pedido al restaurante
+# Id del pedido
+pedido = response.json().get("id")
+# Id del cliente que posee el pedido
+cliente = response.json().get("idCliente")
+# Estado del pedido del cliente 
+estado = response.json().get("estado")
+# Log del estado del pedido solicitado
+Log.write(str(now)+" el pedido "+str(pedido)+" del cliente "+str(cliente)+" se encuentra en el estado "+str(estado)+" "+"\n")
+
+#-----------------------------Enviar pedido al restaurante
 
 # Cierra Archivo de Logs
 Log.close() 
