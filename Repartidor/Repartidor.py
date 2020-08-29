d9ff7857533322614954a8ef10ca698312966a4d
Orlando <batres.93@gmail.com>
2020-08-28 21:44:33 -0600

    Cambio de tipo en el repartidor, ahora es tipo cliente

---
+++ Repartidor/Repartidor.py
@@ -0,0 +1,6 @@
+# Libreria que se utiliza para el envio de datos por medio de JSON
+import json
+# Libreria que permite realizar HTTP requests
+import requests
+# Libreria que obtiene el dia y la hora 
+from datetime import datetime
