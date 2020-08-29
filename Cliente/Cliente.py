237a4b0572aad4f7ab37e64241a2a4c4afe443fa
Orlando <batres.93@gmail.com>
2020-08-28 18:12:30 -0600

    Agregado el log del cliente, y solicitud de pedido

--- Cliente/Cliente.py
+++ Cliente/Cliente.py
@@ -2,10 +2,22 @@
 import json
 # Libreria que permite realizar HTTP requests
 import requests
+# Libreria que obtiene el dia y la hora 
+from datetime import datetime
 
-urlSolicitarPedido = "http://localhost:51630/api/pedido/3"
+# Archivo de Logs
+Log=open("LogCliente.txt","w") 
+#-----------------------------Solicitar pedido al restaurante---------------
+# URL para solicitar pedido
+urlSolicitarPedido = "http://localhost:51630/api/pedido/"
+# Id del cliente que solicita pedido
+idCliente = "3"
+# Request que solicita el pedido al servidor del restaurante
+response = requests.get(urlSolicitarPedido+idCliente)
+# Fecha y hora actual
+now = datetime.now()
+# Log del pedido solicitado
+Log.write(str(now)+" "+"Pedido solicitado "+str(response.json())+" "+"\n") 
 
-
-response = requests.get(urlSolicitarPedido)
-
-print(response.text)
+# Cierra Archivo de Logs
+Log.close() 
